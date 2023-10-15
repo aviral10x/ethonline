9ee3ae7bd133c564d6fecd761a3d41115d302294
AVIRAL GUPTA <91749530+aviral10x@users.noreply.github.com>
2023-10-15 12:34:01 +0530

    test init

--- frontend/pages/api/compile.js
+++ frontend/pages/api/compile.js
@@ -51,7 +51,7 @@
 export default async function handler(req, res) {
   try {
     const { exec } = require("child_process");
-    exec("nargo compile", (error, stdout, stderr) => {
+    exec("nargo init", (error, stdout, stderr) => {
       if (error) {
         console.error(`Error: ${error.message}`);
         res.status(500).json({ error: "Compilation failed" });
